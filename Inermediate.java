ee09493f960c809860f6d57dfd3f1f693188402a
Alex <o.pylkevych@gmail.com>
2014-09-02 20:42:28 +0300

    All intermediate tasks resolved
    Lucky tickets implemented

--- Inermediate.java
+++ Inermediate.java
@@ -11,11 +11,20 @@ public class Intermediate {
     public static void main(String[] args) {
         Intermediate intermediate = new Intermediate();
         intermediate.sumDigits();
+        System.out.println();
         intermediate.countLuckyTickets();
     }
 
-    //TODO: implement method
     private void countLuckyTickets() {
+        int count = 0;
+        for(int i=1; i<= 999999; i++) {
+            int left = i / 100000 + i % 100000 / 10000 + i % 10000 / 1000;
+            int right = i  % 1000 / 100 + i % 100 / 10 + i % 10;
+            if(left == right) {
+                count++;
+            }
+        }
+        System.out.println("Number of lucky tickets is " + count);
     }
     private void sumDigits() {
         Scanner userInput = new Scanner(System.in);
