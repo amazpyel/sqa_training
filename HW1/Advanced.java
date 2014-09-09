c6fd043101ebc6c9eb60b75ab58abce0486a52f2
Alex <o.pylkevych@gmail.com>
2014-09-09 15:12:58 +0300

    Some refactor
    - Packages added
    - All methods are static now
    - Code formated
    - Header with description added
    - array2array2d() implemented

--- HW1/Advanced.java
+++ HW1/Advanced.java
@@ -1,33 +1,44 @@
+package HW1;
+
 import java.util.ArrayList;
 
 /**
  * Created with IntelliJ IDEA.
- * User: pylkevych
+ * Author: Oleksandr Pylkevych o.pylkevych@gmail.com
  * Date: 9/1/14
  * Time: 12:09 PM
- * To change this template use File | Settings | File Templates.
+ * <p/>
+ * Description:
+ * - Multiply two matrices.
+ * - Sort two-dimensional array (in ascending order).
+ * - Print Fibonacci numbers (in a certain amount of a given variable).
  */
 public class Advanced {
     public static void main(String[] args) {
-        Advanced advanced = new Advanced();
         int[][] array2d = {
                 {3, 2, 1},
                 {4, 5, 3}
         };
 
-        int[] array = advanced.array2dToArray(array2d);
-        System.out.println();
-        int[] arraySorted = advanced.sortAscArray(array);
-        System.out.println();
+        int[] array = array2dToArray(array2d);
+        int[] arraySorted = sortAscArray(array);
+        int[][]arraySorted2d = arrayToArray2d(arraySorted, array2d.length, array2d[0].length);
+        System.out.println("Sorted 2D Array: ");
+        for (int row = 0; row < arraySorted2d.length; row++) {
+            for (int column = 0; column < arraySorted2d[row].length; column++) {
+                System.out.print(arraySorted2d[row][column] + " ");
+            }
+            System.out.println();
+        }
         System.out.println();
         System.out.println("Fibonacci numbers:");
-        advanced.fibonacci(10);
+        fibonacci(10);
         System.out.println();
         System.out.println();
-        advanced.multiplyMatrix();
+        multiplyMatrix();
     }
 
-    private int[] array2dToArray(int[][] array2d) {
+    private static int[] array2dToArray(int[][] array2d) {
         ArrayList<Integer> arrayList = new ArrayList<Integer>();
         for (int row = 0; row < array2d.length; row++) {
             for (int column = 0; column < array2d[row].length; column++) {
@@ -37,18 +48,20 @@ public class Advanced {
         int[] ret = new int[arrayList.size()];
         for (int i = 0; i < ret.length; i++) {
             ret[i] = arrayList.get(i).intValue();
-            System.out.print(ret[i] + " ");
         }
         return ret;
     }
 
-    //TODO: finish method
-    public int[][] arrayToArray2d(int[] arrayList, int row, int column) {
-        int[][] array2d = new int[row][column];
+    public static int[][] arrayToArray2d(int[] array, int rows, int columns) {
+        if (array.length != (rows*columns))
+            throw new IllegalArgumentException("Invalid array length");
+        int[][] array2d = new int[rows][columns];
+        for ( int i = 0; i < rows; i++ )
+            System.arraycopy(array, (i*columns), array2d[i], 0, columns);
         return array2d;
     }
 
-    private int[] sortAscArray(int[] array) {
+    private static int[] sortAscArray(int[] array) {
         boolean swapped = true;
         int temp;
         do {
@@ -62,27 +75,23 @@ public class Advanced {
                 }
             }
         } while (swapped);
-        System.out.println("Sorted array:");
-        for (int h = 0; h < array.length; h++) {
-            System.out.print(array[h] + " ");
-        }
         return array;
     }
 
-    private void fibonacci(int n) {
+    private static void fibonacci(int n) {
         int[] fibonacci = new int[n];
         for (int i = 0; i < fibonacci.length; i++) {
-            if(i < 2) {
+            if (i < 2) {
                 fibonacci[i] = 1;
-            }
-            else {
+            } else {
                 fibonacci[i] = fibonacci[i - 2] + fibonacci[i - 1];
             }
             System.out.print(fibonacci[i] + " ");
         }
     }
 
-    private void multiplyMatrix() {
+    //algorithm from Wikipedia
+    private static void multiplyMatrix() {
         int m = 3;
         int n = 4;
         int q = 5;
@@ -99,15 +108,13 @@ public class Advanced {
                 {1, 2, 3, 4, 5}
         };
         int[][] C = new int[m][q];
-
-        for(int r = 0; r < n; r++) {
-            for(int i = 0; i < m; i++) {
+        for (int r = 0; r < n; r++) {
+            for (int i = 0; i < m; i++) {
                 for (int j = 0; j < q; j++) {
                     C[i][j] += A[i][r] * B[r][j];
                 }
             }
         }
-
         System.out.println("Product of matrices: ");
         for (int row = 0; row < C.length; row++) {
             for (int column = 0; column < C[row].length; column++) {
@@ -115,6 +122,5 @@ public class Advanced {
             }
             System.out.println();
         }
-
     }
 }
